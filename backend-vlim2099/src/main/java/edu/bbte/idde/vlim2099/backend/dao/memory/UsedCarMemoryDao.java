e2fda0ff99df6fa37f9eff2afc7d29743d43e275
Velican László <velicanlaszlo01@gmail.com>
2022-10-26 11:38:44 +0300

    Lab1_javitas1

--- backend-vlim2099/src/main/java/edu/bbte/idde/vlim2099/backend/dao/memory/UsedCarMemoryDao.java
+++ backend-vlim2099/src/main/java/edu/bbte/idde/vlim2099/backend/dao/memory/UsedCarMemoryDao.java
@@ -2,6 +2,8 @@ package edu.bbte.idde.vlim2099.backend.dao.memory;
 
 import edu.bbte.idde.vlim2099.backend.dao.UsedCarDao;
 import edu.bbte.idde.vlim2099.backend.model.UsedCar;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Collection;
 import java.util.Map;
@@ -12,10 +14,15 @@ public class UsedCarMemoryDao implements UsedCarDao {
 
     private static final Map<Long, UsedCar> USED_CAR_ENTITIES = new ConcurrentHashMap<>();
     private static final AtomicLong ID_GENERATOR = new AtomicLong();
+    private static final Logger LOG = LoggerFactory.getLogger(UsedCarMemoryDao.class);
 
     @Override
     public UsedCar findById(Long usedCarId) {
-        return USED_CAR_ENTITIES.get(usedCarId);
+        UsedCar searchedCar = USED_CAR_ENTITIES.get(usedCarId);
+        if (searchedCar != null) {
+            LOG.info("The car with id {} has been found",usedCarId);
+        }
+        return searchedCar;
     }
 
     @Override
@@ -23,21 +30,24 @@ public class UsedCarMemoryDao implements UsedCarDao {
         Long currentUsedCarId = ID_GENERATOR.getAndIncrement();
         usedCar.setId(currentUsedCarId);
         USED_CAR_ENTITIES.put(currentUsedCarId, usedCar);
-
+        LOG.info("New car with id {} has been created",currentUsedCarId);
     }
 
     @Override
     public Collection<UsedCar> findAllUsedCar() {
+        LOG.info("All cars has been found");
         return USED_CAR_ENTITIES.values();
     }
 
     @Override
     public void updateUsedCar(UsedCar usedCar, Long id) {
+        LOG.info("The car with id {} has been updated", id);
         USED_CAR_ENTITIES.get(id).setCar(usedCar);
     }
 
     @Override
     public void deleteUsedCar(Long id) {
+        LOG.info("The car with id {} has been deleted", id);
         USED_CAR_ENTITIES.remove(id);
     }
 }
