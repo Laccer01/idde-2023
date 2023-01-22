b96e006d145dc1a91249963cebbec4e3e7ec5dc0
Velican László <velicanlaszlo01@gmail.com>
2023-01-23 00:36:35 +0200

    Lab5_javitas2

---
+++ spring-vlim2099/src/main/java/edu/bbte/idde/vlim2099/spring/controller/UsedCarAndUsedCarOwnerController.java
@@ -0,0 +1,82 @@
+package edu.bbte.idde.vlim2099.spring.controller;
+
+import edu.bbte.idde.vlim2099.spring.controller.dto.exception.NotFoundException;
+import edu.bbte.idde.vlim2099.spring.controller.dto.incoming.UsedCarCreationDto;
+import edu.bbte.idde.vlim2099.spring.controller.dto.outgoing.UsedCarResponseDto;
+import edu.bbte.idde.vlim2099.spring.controller.mapper.UsedCarMapper;
+import edu.bbte.idde.vlim2099.spring.dao.UsedCarOwnerDao;
+import edu.bbte.idde.vlim2099.spring.dao.model.UsedCar;
+import edu.bbte.idde.vlim2099.spring.dao.model.UsedCarOwner;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Collection;
+import java.util.Objects;
+
+@Slf4j
+@RestController
+@RequestMapping("/usedCarAndUsedCarOwner")
+public class UsedCarAndUsedCarOwnerController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(UsedCarController.class);
+    @Autowired
+    private UsedCarOwnerDao usedCarOwnerDao;
+    @Autowired
+    private UsedCarMapper usedCarMapper;
+
+    @GetMapping("/{id}/usedCars")
+    public Collection<UsedCarResponseDto> findUsedCarOwnersById(@PathVariable("id") Long id) {
+        UsedCarOwner result = usedCarOwnerDao.getById(id);
+
+        if (result == null) {
+            throw new NotFoundException();
+        }
+
+        return usedCarMapper.modelsToDtos(result.getUsedCars());
+    }
+
+    @PostMapping("/{id}/usedCars")
+    public UsedCarResponseDto createUsedCar(@RequestBody @Valid UsedCarCreationDto dto,
+                                            @PathVariable("id") Long id) {
+        LOGGER.info("The new used car for owner with id: {} have been created successfully!", id);
+
+        UsedCar newUsedCar = usedCarMapper.creationDtoToModel(dto);
+
+        UsedCarOwner usedCarOwner = usedCarOwnerDao.getById(id);
+        Collection<UsedCar> usedCarsFromOwner = usedCarOwner.getUsedCars();
+        usedCarsFromOwner.add(newUsedCar);
+        usedCarOwner.setUsedCars(usedCarsFromOwner);
+
+        newUsedCar.setUsedCarOwner(usedCarOwner);
+
+        usedCarOwnerDao.saveAndFlush(usedCarOwner);
+
+        return usedCarMapper.modelToDto(newUsedCar);
+    }
+
+    @DeleteMapping("/{id}/usedCars/{usedCarId}")
+    public void deleteUsedCar(@PathVariable("id") Long id, @PathVariable("usedCarId") Long usedCarId) {
+        UsedCarOwner usedCarOwner = usedCarOwnerDao.getById(id);
+
+        Collection<UsedCar> usedCars = usedCarOwner.getUsedCars();
+        UsedCar usedCar = null;
+        for (UsedCar iterator : usedCars) {
+            if (Objects.equals(iterator.getId(), usedCarId)) {
+                usedCar = iterator;
+            }
+        }
+
+        usedCars.remove(usedCar);
+        usedCarOwner.setUsedCars(usedCars);
+        if (usedCar != null) {
+            usedCar.setUsedCarOwner(null);
+        }
+
+        usedCarOwnerDao.saveAndFlush(usedCarOwner);
+
+    }
+}
--- spring-vlim2099/src/main/java/edu/bbte/idde/vlim2099/spring/controller/UsedCarOwnersController.java
+++ spring-vlim2099/src/main/java/edu/bbte/idde/vlim2099/spring/controller/UsedCarOwnersController.java
@@ -1,15 +1,9 @@
 package edu.bbte.idde.vlim2099.spring.controller;
 
-import edu.bbte.idde.vlim2099.spring.controller.dto.exception.NotFoundException;
-import edu.bbte.idde.vlim2099.spring.controller.dto.incoming.UsedCarCreationDto;
 import edu.bbte.idde.vlim2099.spring.controller.dto.incoming.UsedCarOwnerCreationDto;
 import edu.bbte.idde.vlim2099.spring.controller.dto.outgoing.UsedCarOwnerResponseDto;
-import edu.bbte.idde.vlim2099.spring.controller.dto.outgoing.UsedCarResponseDto;
-import edu.bbte.idde.vlim2099.spring.controller.mapper.UsedCarMapper;
 import edu.bbte.idde.vlim2099.spring.controller.mapper.UsedCarOwnerMapper;
-import edu.bbte.idde.vlim2099.spring.dao.UsedCarDao;
 import edu.bbte.idde.vlim2099.spring.dao.UsedCarOwnerDao;
-import edu.bbte.idde.vlim2099.spring.dao.model.UsedCar;
 import edu.bbte.idde.vlim2099.spring.dao.model.UsedCarOwner;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
@@ -29,15 +23,8 @@ public class UsedCarOwnersController {
     @Autowired
     private UsedCarOwnerDao usedCarOwnerDao;
     @Autowired
-    private UsedCarDao usedCarDao;
-
-    @Autowired
-    private UsedCarMapper usedCarMapper;
-    @Autowired
     private UsedCarOwnerMapper usedCarOwnerMapper;
 
-    // ha megvan adva paraméternek a brand név akkor meghívodik a szűrés, ellenkező
-    // esetben listázodik az összes autó
     @GetMapping
     @ResponseBody
     public Collection<UsedCarOwnerResponseDto> findAll(
@@ -56,44 +43,12 @@ public class UsedCarOwnersController {
         return usedCarOwnerMapper.modelToDto(usedCarOwnerDao.getById(id));
     }
 
-    @GetMapping("/{id}/usedCars")
-    public Collection<UsedCarResponseDto> findUsedCarOwnersById(@PathVariable("id") Long id) {
-        UsedCarOwner result = usedCarOwnerDao.getById(id);
-
-        if (result == null) {
-            throw new NotFoundException();
-        }
-
-        return usedCarMapper.modelsToDtos(result.getUsedCars());
-    }
-
     @PostMapping
     public UsedCarOwnerResponseDto  create(@RequestBody @Valid UsedCarOwnerCreationDto dto) {
         LOGGER.info("The new used car owner have been created successfully!");
         UsedCarOwner newUsedCarOwner = usedCarOwnerMapper.creationDtoToModel(dto);
         usedCarOwnerDao.saveAndFlush(newUsedCarOwner);
         return usedCarOwnerMapper.modelToDto(newUsedCarOwner);
-
-    }
-
-    @PostMapping("/{id}/usedCars")
-    public UsedCarResponseDto createUsedCar(@RequestBody @Valid UsedCarCreationDto dto,
-                                            @PathVariable("id") Long id) {
-        LOGGER.info("The new used car for owner with id: {} have been created successfully!", id);
-
-        UsedCar newUsedCar = usedCarMapper.creationDtoToModel(dto);
-
-        UsedCarOwner usedCarOwner = usedCarOwnerDao.getById(id);
-        Collection<UsedCar> usedCarsFromOwner = usedCarOwner.getUsedCars();
-        usedCarsFromOwner.add(newUsedCar);
-        usedCarOwner.setUsedCars(usedCarsFromOwner);
-
-        newUsedCar.setUsedCarOwner(usedCarOwner);
-
-        usedCarDao.saveAndFlush(newUsedCar);
-        usedCarOwnerDao.saveAndFlush(usedCarOwner);
-
-        return usedCarMapper.modelToDto(newUsedCar);
     }
 
     @PutMapping("/{id}")
@@ -110,21 +65,6 @@ public class UsedCarOwnersController {
         UsedCarOwner usedCarOwner = usedCarOwnerDao.getById(usedCarOwnerId);
         usedCarOwnerDao.delete(usedCarOwner);
 
-    }
-
-    @DeleteMapping("/{id}/usedCars/{usedCarId}")
-    public void deleteUsedCar(@PathVariable("id") Long id, @PathVariable("usedCarId") Long usedCarId) {
-        UsedCarOwner usedCarOwner = usedCarOwnerDao.getById(id);
-        UsedCar usedCar = usedCarDao.getById(usedCarId);
-
-        Collection<UsedCar> usedCars = usedCarOwner.getUsedCars();
-        usedCars.remove(usedCar);
-        usedCarOwner.setUsedCars(usedCars);
-        usedCarOwnerDao.saveAndFlush(usedCarOwner);
-
-        usedCar.setUsedCarOwner(null);
-        usedCarDao.saveAndFlush(usedCar);
-        usedCarDao.delete(usedCar);
     }
 
 }
