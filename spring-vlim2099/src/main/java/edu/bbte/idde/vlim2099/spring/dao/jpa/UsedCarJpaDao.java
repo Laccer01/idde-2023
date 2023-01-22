0746ff252e0ae38b8313d3171eb40ccc0bb38882
Velican László <velicanlaszlo01@gmail.com>
2023-01-22 23:25:55 +0200

    Lab5_javitas1

--- spring-vlim2099/src/main/java/edu/bbte/idde/vlim2099/spring/dao/jpa/UsedCarJpaDao.java
+++ spring-vlim2099/src/main/java/edu/bbte/idde/vlim2099/spring/dao/jpa/UsedCarJpaDao.java
@@ -5,19 +5,10 @@ import edu.bbte.idde.vlim2099.spring.dao.model.UsedCar;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.Collection;
 
 @Repository
 @Profile("jpa")
 public interface UsedCarJpaDao extends UsedCarDao, JpaRepository<UsedCar, Long> {
-
-    @Modifying
-    @Override
-    @Query("select usedCar from UsedCar usedCar where usedCar.brand = ?1")
-    Collection<UsedCar> findByBrand(String brand);
 
 }
--- spring-vlim2099/src/main/java/edu/bbte/idde/vlim2099/spring/dao/jpa/UsedCarOwnerJpaDao.java
+++ spring-vlim2099/src/main/java/edu/bbte/idde/vlim2099/spring/dao/jpa/UsedCarOwnerJpaDao.java
@@ -4,19 +4,12 @@ import edu.bbte.idde.vlim2099.spring.dao.UsedCarOwnerDao;
 import edu.bbte.idde.vlim2099.spring.dao.model.UsedCarOwner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
-import java.util.Collection;
+import org.springframework.stereotype.Repository;
 
 @Repository
 @Profile("jpa")
 
 public interface UsedCarOwnerJpaDao extends UsedCarOwnerDao, JpaRepository<UsedCarOwner, Long> {
 
-    @Modifying
-    @Override
-    @Query("select usedCarOwner from UsedCarOwner usedCarOwner where usedCarOwner.lastName = ?1")
-    Collection<UsedCarOwner> findByLastName(String lastName);
 }
